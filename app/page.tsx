81dae0dc7b70b9e254fbdc214b13203fe82c0fad
inuldev <inuldev0@gmail.com>
2025-06-28 04:21:21 +0700

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/hero";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Home Page</h1>
+      <Hero />
     </div>
   );
 }
